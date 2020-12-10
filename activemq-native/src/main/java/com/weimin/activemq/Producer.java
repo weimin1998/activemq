1f766b0ce87431f0d713873ecf579d8ac8bc80f0
weimin <2561394933@qq.com>
2020-12-10 18:18:08 +0800

    activemq 生产者 入门程序

---
+++ activemq-native/src/main/java/com/weimin/activemq/Producer.java
@@ -0,0 +1,46 @@
+package com.weimin.activemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.UUID;
+
+public class Producer {
+
+    public static void main(String[] args) throws JMSException {
+        // 创建连接工厂，指定连接到那个activemq
+        ActiveMQConnectionFactory activeMQConnectionFactory =
+                new ActiveMQConnectionFactory(
+                        "admin",
+                        "admin",
+                        "tcp://123.56.169.57:61616");
+
+        // 创建一个连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+
+        // 创建一个session 第一个参数表示是否开启事务，第二个参数表示签收方式
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // 创建目的地
+        Destination queue = session.createQueue("queue01");
+
+        // 创建消息生产者
+        MessageProducer producer = session.createProducer(queue);
+
+        //创建一条文本消息
+        TextMessage textMessage = session.createTextMessage("文本消息:"+ UUID.randomUUID().toString());
+
+        // 发送
+        producer.send(textMessage);
+
+        producer.close();
+        session.close();
+        connection.close();
+
+        System.out.println("发送完成");
+
+    }
+}
